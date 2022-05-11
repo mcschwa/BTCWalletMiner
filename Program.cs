d4a0f9d8ff97f2eb37efcaf095e6b47ca80dd9a0
Schwaaa <rickhousemen2@gmail.com>
2022-05-11 14:54:02 +0200

    v0.1
    -Logo added
    -Main structure

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using ProgramSettings;
+using ProgramBitcoin;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		var Settings = new Settings();
+		Console.WriteLine(Settings.ProgramLogo);
+	}
+}
---
+++ classes/Bitcoin.cs
@@ -0,0 +1,14 @@
+namespace ProgramBitcoin
+{
+	public class BitcoinAddress
+	{
+		public int minLength = 26;
+		public int maxLength = 35;
+
+	}
+
+	public class BitcoindPrivateKey
+	{
+		public Int32 length = 256;
+	}
+}
